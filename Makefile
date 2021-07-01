0e077012c75f97a9f90835af14c5418d7b085377
Gary Bake <garybake@gmail.com>
2021-07-01 22:51:59 +0100

    Added more services and dockerise service app

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 JUPYTER_DIR := ${CURDIR}/jupyter
+SERVICE_DIR := ${CURDIR}/service
 
 jupyter_build:
 	docker build -t garyb/anomalydetect ${JUPYTER_DIR}
@@ -6,5 +7,11 @@ jupyter_build:
 jupyter_run:
 	docker run --rm -p 8080:8080 -v ${JUPYTER_DIR}:/src garyb/anomalydetect
 
+service_build:
+	docker build -t garyb/lp-service ${SERVICE_DIR}
+
 service_run:
+	docker run --rm -p 8000:8000 -v ${SERVICE_DIR}:/src garyb/lp-service
+
+service_run_local:
 	cd ./service && uvicorn main:app --reload
--- service/main.py
+++ service/main.py
@@ -1,17 +1,25 @@
 from typing import Optional, List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from joblib import load
 from pydantic import BaseModel
 
 MODEL_FILE ='model.joblib'
 
-app = FastAPI()
+app = FastAPI(    
+    title="Anomaly Detection Platform",
+    description="This is a very fancy project to detect anomalies",
+    version="0.0.1",
+)
 app.ml_model = load(MODEL_FILE)
 
-class Item(BaseModel):
+class PredictIn(BaseModel):
     feature_vector: List[float]
-    score: Optional[bool] = None
+    score: Optional[bool] = False
+
+class PredictOut(BaseModel):
+    is_inlier: bool
+    anomaly_score: Optional[float] = None
 
 
 @app.get("/")
@@ -19,13 +27,30 @@ async def root():
     return {"message": "Welcome to prediction world!"}
 
 
-@app.post("/prediction")
-async def prediction(item: Item):
-    pred = get_prediction(item.feature_vector)
-    return {"is_inlier": str(pred)}
+@app.post("/prediction", response_model=PredictOut)
+async def post_prediction(pred_in: PredictIn):
+    if len(pred_in.feature_vector) != 2:
+        raise HTTPException(status_code=404, detail="Invalid feature vector, should be [float, float]")
+
+    pred = get_prediction(pred_in.feature_vector)
+    ret = {"is_inlier": pred}
+
+    if pred_in.score:
+        score = get_score(pred_in.feature_vector)
+        ret['anomaly_score'] = score
+
+    return ret
+
+@app.get('/model_information')
+async def get_model_information():
+    return app.ml_model.get_params()
 
 
-def get_prediction(feature_vector: List[float])-> bool :
+def get_prediction(feature_vector: List[float]) -> bool :
     # TODO accept array of vectors
     pred = app.ml_model.predict([feature_vector])
     return pred[0] == 1
+
+def get_score(feature_vector: List[float]) -> float :
+    scores = app.ml_model.score_samples([feature_vector])
+    return scores[0]
