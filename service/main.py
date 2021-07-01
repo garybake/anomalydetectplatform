fa1232265977877bc64d4161d84245e9e9d7eb47
Gary Bake <garybake@gmail.com>
2021-07-01 08:45:11 +0100

    Add prediction service to server

--- service/main.py
+++ service/main.py
@@ -1,13 +1,31 @@
+from typing import Optional, List
+
 from fastapi import FastAPI
+from joblib import load
+from pydantic import BaseModel
+
+MODEL_FILE ='model.joblib'
 
 app = FastAPI()
+app.ml_model = load(MODEL_FILE)
+
+class Item(BaseModel):
+    feature_vector: List[float]
+    score: Optional[bool] = None
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to prediction world!"}
 
 
 @app.post("/prediction")
-async def prediction():
-    return {"message": "prediction World"}
+async def prediction(item: Item):
+    pred = get_prediction(item.feature_vector)
+    return {"is_inlier": str(pred)}
+
+
+def get_prediction(feature_vector: List[float])-> bool :
+    # TODO accept array of vectors
+    pred = app.ml_model.predict([feature_vector])
+    return pred[0] == 1
